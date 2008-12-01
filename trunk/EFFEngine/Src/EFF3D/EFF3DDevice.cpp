be9e62cd854d9a2d6aa40348805373b43a45ab5e
chena.invalid <chena.invalid@a542bc0e-bb03-11dd-bba1-afcbec1a1e1f>
2008-12-01 15:26:38 +0000

    git-svn-id: https://eff.googlecode.com/svn@35 a542bc0e-bb03-11dd-bba1-afcbec1a1e1f

---
+++ trunk/EFFEngine/Src/EFF3D/EFF3DDevice.cpp
@@ -0,0 +1,12 @@
+/******************************************************************************
+	created:	2008-12-1   23:24
+	file path:	d:\EFF\EFFEngine\Src\EFF3D\EFF3DDevice.cpp
+	author:		ChenA
+	
+	purpose:	
+******************************************************************************/
+
+#include "stdafx.h"
+#include "EFF3DDevice.h"
+
+#define new EFFNEW
