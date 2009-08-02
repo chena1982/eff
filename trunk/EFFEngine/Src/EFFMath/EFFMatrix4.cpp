93ca9f699e2922093194e4bd5629d4da3a00ddb2
chena.invalid <chena.invalid@a542bc0e-bb03-11dd-bba1-afcbec1a1e1f>
2009-08-02 09:23:46 +0000

    git-svn-id: https://eff.googlecode.com/svn@48 a542bc0e-bb03-11dd-bba1-afcbec1a1e1f

---
+++ trunk/EFFEngine/Src/EFFMath/EFFMatrix4.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+	created:	2008-12-13   0:48
+	file path:	d:\EFF\EFFEngine\Src\EFFMath\EFFMatrix4.cpp
+	author:		ChenA
+	
+	purpose:	
+******************************************************************************/
+
+#include "stdafx.h"
+#include "EFFMatrix4.h"
+
+#define new EFFNEW
+
+EFFMatrix4::EFFMatrix4()
+{
+}
+
+EFFMatrix4::~EFFMatrix4()
+{
+}
---
+++ trunk/EFFEngine/Src/EFFMath/EFFRect.cpp
@@ -0,0 +1,14 @@
+/******************************************************************************
+	created:	2008-12-13   0:35
+	file path:	d:\EFF\EFFEngine\Src\EFFMath\EFFRect.cpp
+	author:		ChenA
+	
+	purpose:	
+******************************************************************************/
+
+#include "stdafx.h"
+#include "EFFRect.h"
+
+#define new EFFNEW
+
+
