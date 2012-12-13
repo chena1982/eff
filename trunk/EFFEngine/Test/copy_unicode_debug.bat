10e8beae0fb5973320220494f53575269f682e0f
ChenA <chena.invalid@gmail.com>
2012-12-13 22:08:02 +0800

    目录调整

--- trunk/EFFEngine/Test/copy_unicode_debug.bat
+++
@@ -1,3 +0,0 @@
-xcopy ..\Dll\Unicode_Debug\*.dll .\Bin\Unicode_Debug /s /r /y
-xcopy ..\Src\EFF3D\EFF3D.py\.dll .\Bin\Unicode_Debug /s /r /y
-pause
--- trunk/EFFEngine/copy_unicode_debug.bat
+++
@@ -1,3 +0,0 @@
-xcopy .\Temp\Lib\Unicode_Debug\*.dll .\Build\Bin\ /s /r /y
-xcopy .\Temp\Lib\Unicode_Debug\*.pyd .\Build\Bin\ /s /r /y
-pause
