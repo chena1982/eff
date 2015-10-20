5416fe7799c959901a0d10b6ca795ec94f929294
ChenA <chena.invalid@gmail.com>
2015-10-21 00:24:14 +0800

    mygui支持

---
+++ Src/EFF3D/EFF3DVertexBuffer.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+	created:	2014-01-01   1:51
+	file path:	d:\EFF\Src\EFF3D\EFF3DVertexBuffer.cpp
+	author:		ChenA
+	
+	purpose:	
+******************************************************************************/
+
+#include "EFF3DPCH.h"
+#include "EFF3DVertexBuffer.h"
+
+//#define new EFFNEW
+
+EFF3D_BEGIN
+
+RTTI_IMPLEMENT_PURE(EFF3DVertexBuffer, 0)
+
+EFF3D_END
---
+++ copy_mygui.bat
@@ -0,0 +1,5 @@
+xcopy D:\GitHub\mygui\build\lib\Debug\*.lib .\ThirdPartyLib\Lib\VS2013\Debug\ /s /r /y
+
+xcopy D:\GitHub\mygui\build\bin\debug\*.dll .\Bin\ /s /r /y
+
+pause
